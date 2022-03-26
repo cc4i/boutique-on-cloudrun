60d68a37305225dddcf7c3486a24c2e28cdac3fc
Chuan <chuancc@google.com>
2022-03-26 11:39:05 +0800

    inital commit

---
+++ hack/setup.sh
@@ -0,0 +1,4 @@
+# 1. Launch services in Cloud Run / min 10
+# 2. Provision adjcent systems, such as Redis//
+# 3. Extract related pramameters and update launched services with right parameters.
+# 4. 
